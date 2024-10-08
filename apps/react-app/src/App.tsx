d5e69a86eba7156baa1cf9dbb0130e795f88b793
Tavera <f.tavera@accenture.com>
2024-10-08 17:33:56 -0600

    sesion 1 act 2

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -1,9 +1,7 @@
-import { Grid } from "@mui/material";
-
 import { HomePage } from "./components/Page";
-import { PageContainer } from "./components/Page/LoginPage/LoginPage.styles";
 import { PostPage } from "./components/Page";
 import { CategoriesPage } from "./components/Page";
+import LoginPage from "./components/Page/LoginPage";
 
 
 function App() {
@@ -13,13 +11,7 @@ function App() {
       <PostPage />
       <CategoriesPage />
       
-      {/* ACT 2 - Move the following content to a new component called LoginPage and render it*/}
-      <PageContainer container>
-        Login Page
-        <Grid item md={4} xs={4} lg={4}>
-          Form
-        </Grid>
-      </PageContainer>
+      <LoginPage />
     </>
   );
 }
